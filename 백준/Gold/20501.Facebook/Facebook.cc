27eb8f5d1d2a4a6e3d87c9a8ab8b85318ab4e3d5
Jaehong Kim <84514047+hongjae95@users.noreply.github.com>
2022-01-23 00:21:30 +0900

    [Gold II] Title: Facebook, Time: 276 ms, Memory: 2528 KB -BaekjoonHub

--- 백준/Gold/20501.Facebook/Facebook.cc
+++ 백준/Gold/20501.Facebook/Facebook.cc
@@ -1,33 +1,24 @@
-#include <iostream>
-#include <string>
-#include <bitset>
-#define MAX 2001
-
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
 using namespace std;
 
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	bitset<2001> bs[MAX];
+
+int main()
+{
+	fastio;
 	int n;
 	cin >> n;
-	for(int i = 0; i < n; ++i){
-		string s;
-		cin >> s;
-		for(int j = 0; j < n; ++j){
-			if(s[j] == '1'){
-				bs[i].set(j);
-			}
-		}
-	}
+	vector<bitset<2000>> bv(n);
+	for (int i = 0; i < n; i++)
+		cin >> bv[i];
+
 	int q;
 	cin >> q;
-	for(int k = 0; k < q; ++k){
-		int x, y;
-		cin >> x >> y;
-		--x;
-		--y;
-		cout << (bs[x] & bs[y]).count() << '\n';
+	while (q--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << (bv[a - 1] & bv[b - 1]).count() << '\n';;
 	}
-	return 0;
 }
+	
